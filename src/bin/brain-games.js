9351a8b5021ad6f6287b062b458533055c5a62c5
Ilya Grachev <ilya095@gmail.com>
2019-04-29 22:24:50 +0300

    add username request

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+
+import askUserName from '..';
+
 console.log('Welcome to the Brain Games!');
+
+askUserName();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import readlineSync from 'readline-sync';
+
+const askUserName = () => {
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+};
+export default askUserName;
