c478038324a3974b1c0344bcecd03bcf56d40a75
Ilya Grachev <ilya095@gmail.com>
2019-05-13 12:24:03 +0300

    readability fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const numberOfRounds = 3;
 
-const getRandomInt = (min = 1, max = 50) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomInt = (min = 1, max = 30) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
@@ -11,16 +11,18 @@ const getName = () => {
 };
 
 const runGame = (gameRules, getDataGame) => {
-  console.log(`Welcome to the Brain Games!\n${gameRules}\n`);
+  console.log('Welcome to the Brain Games!');
+  console.log(`${gameRules}`);
   const userName = getName();
   for (let i = 1; i <= numberOfRounds; i += 1) {
-    const data = getDataGame();
-    console.log(`Question: ${data.question}`);
+    const [question, currentAnswer] = getDataGame();
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (String(data.currentAnswer) === userAnswer) {
+    if (currentAnswer === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${String(data.currentAnswer)}'.\nLet's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${currentAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
